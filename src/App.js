6706b5cc496252e0e7d7051b88bceda1915a7fda
l-Bohdan-l <bpapushoj@gmail.com>
2021-11-30 23:01:59 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Profile user={userData} />
-        <Statistics stats={data} />
+        <Statistics title="Upload stats" stats={data} />
         <FriendsList friends={friends} />
         <Transactions items={items} />
       </header>
